ada148f322f701e5d4087325f8ddd494099fa96e
Stephanie Rapisardo <sjrapisardo@gmail.com>
2019-04-25 19:58:17 -0500

    Continue working on blogz

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, session, flash
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
